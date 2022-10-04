c34257fba5fefb0c44e628aa2512ee8954289593
rianl <rianl@DESKTOP-QEKN3KQ>
2022-10-04 00:16:47 -0300

    Conta Bancaria

--- primeiroProg/src/primeiroProg/JavaProg/JavaProg.java
+++ primeiroProg/src/primeiroProg/JavaProg/JavaProg.java
@@ -1,15 +1,15 @@
 package primeiroProg.JavaProg;
 
+import java.util.ArrayList;
+
 import JavaProg.classes.Conta;
 
 public class JavaProg {
 	public static void main(String[] args) {
 		
-		Conta conta1 =  new Conta("rian", 20);
+		ArrayList<Conta> clientes = new ArrayList<Conta>();
 		
-		conta1.sacar(200);
 		
-		System.out.println(conta1.getSaldo());
 		
 		
 	}
